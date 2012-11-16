d9a4e1c0557a2760eadce81ce03821d6f3de6e02
cyrus <cyrus@lapCyrus>
2012-11-16 16:29:48 +0100

    v3

---
+++ menschv3/src/Models/Figure.java
@@ -0,0 +1,36 @@
+package Models;
+
+public class Figure {
+	private int wegLaenge;
+	private int figureID;
+	private Player player;
+	
+	public Figure(int figID,Player player) {
+	    this.wegLaenge = 0;
+	    this.figureID = figID;
+	    this.player = player;
+	};
+	
+	public int getFigurePos() {
+		return this.wegLaenge;
+	}
+	public void setFigurePos(int position) {
+		wegLaenge = position;
+	}
+	
+	
+	public void resetWegLaenge(){
+	    this.wegLaenge = 0;
+	}
+
+	public int getFigureID() {
+	    return figureID;
+	}
+	
+	public Player getFigurePlayer(){
+	    return this.player;
+	}
+
+	
+	
+}
