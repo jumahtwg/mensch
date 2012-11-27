3694044a61e0e192da588906518778b50dd8dbce
cyrus <cyrus@lapCyrus>
2012-11-27 14:49:56 +0100

    Okay..

--- menschv3/src/Controller/Application.java
+++
@@ -1,9 +0,0 @@
-package Controller;
-
-public class Application {
-
-    public static void main(String[] args) {
-	Controller game = new Controller();
-	game.init();
-    }
-}
--- src/Models/Dice.java
+++
@@ -1,9 +0,0 @@
-package Models;
-
-public class Dice {
-
-	public int roll() {
-		int x =  (int)(((Math.random()*10) % 6)+1);
-		return x;
-	}
-}
