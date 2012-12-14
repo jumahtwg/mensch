7e927812da27e3cd51ac91029b46d21bed371b73
Cyrus <Cyrus@desktopCyrus>
2012-12-14 17:35:56 +0100

    dtgzhrfnghts

---
+++ src/model/Player.java
@@ -0,0 +1,130 @@
+package model;
+
+import java.util.*;
+
+import model.Dice;
+import model.Figure;
+
+public class Player {
+
+	private final int playerID;
+	private Dice dice;
+	private int startField;
+	private int endField;
+
+	private Deque<Figure> startStack = new ArrayDeque<Figure>();
+	private Figure[] pgFigureArray = new Figure[4];
+
+	public Player(int playerID) {
+		this.playerID = playerID;
+		for (int i = 3; i >= 0; i--) {
+			startStack.push(new Figure(i, this));
+		}
+		this.dice = new Dice();
+
+		switch (playerID) {
+		case 0:
+			this.startField = 0;
+			break;
+		case 1:
+			this.startField = 10;
+			break;
+		case 2:
+			this.startField = 20;
+			break;
+		case 3:
+			this.startField = 30;
+			break;
+		default:
+		}
+		this.endField = startField - 1;
+	}
+
+	public int getPlayerID() {
+		return this.playerID;
+	}
+
+	public Figure popFigure() {
+		Figure tmp;
+		//Keine Figuren mehr im Startfeld!
+		if (startStack.isEmpty()) {
+			return null;
+		}
+		tmp = startStack.pop();
+		// Figur " + tmp.getFigureID() + " wurde vom Stack geholt!
+		pgFigureArray[tmp.getFigureID()] = tmp;
+		return tmp;
+	}
+
+	public void pushFigure(Figure figure) {
+		if (startStack.size() == 4) {
+			System.out
+					.println("Alle Figuren des Spielers sind bereits im Startfeld");
+			return;
+		}
+		figure.resetWegLaenge();
+		pgFigureArray[figure.getFigureID()] = null;
+		startStack.push(figure);
+		return;
+	}
+
+	public boolean figureStackEmpty() {
+		if (startStack.isEmpty()) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean figureArrayEmpty(){
+		for(int i = 0; i< pgFigureArray.length; i++){
+			if(pgFigureArray[i] == null)
+				continue;
+			else
+				return false;
+		}
+		return true;
+	}
+
+	public int getStackSize() {
+		return startStack.size();
+	}
+
+	public int rolling() {
+		return dice.roll();
+	}
+
+	public int getStartField() {
+		return this.startField;
+	}
+
+	public Figure getFigure(int figureID) {
+		return pgFigureArray[figureID];
+	}
+
+	public void storeFigure(Figure fig) {
+		for (int i = 0; i < pgFigureArray.length; i++) {
+			if (pgFigureArray[i] == null) {
+				pgFigureArray[i] = fig;
+				return;
+			}
+		}
+	}
+	public void removeFigureFromActiveSoldiers(Figure fig){
+		pgFigureArray[fig.getFigureID()] = null;
+		return;
+	}
+	public Figure[] getPgFigureArray() {
+		return pgFigureArray;
+	}
+	public boolean isFigureAvailable(int figID){
+		if(figID > 3 || pgFigureArray[figID] == null){
+			return false;
+		}else{
+			return true;
+		}
+	}
+	
+	public void hasFigure(Figure fig){
+		
+	}
+}
---
+++ src/model/Playground.java
@@ -0,0 +1,102 @@
+package model;
+
+import java.util.*;
+
+import model.Figure;
+import model.Player;
+
+public class Playground {
+
+	private int anzMit;
+	private Figure fieldArray[];
+	
+	
+	private Figure target1[];
+	private Figure target2[];
+	private Figure target3[];
+	private Figure target4[];
+	
+	private LinkedList<Player> players = new LinkedList<Player>();
+		
+	public Playground() {
+		this.fieldArray = new Figure[40];
+		this.target1 = new Figure[4];
+		this.target2 = new Figure[4];
+		this.target3 = new Figure[4];
+		this.target4 = new Figure[4];
+		this.anzMit = 0;
+	}
+
+	public void addPlayer(Player newPlayer){		
+		players.add(newPlayer);
+		anzMit++;
+	}
+	public int getAnzMit() {
+		return anzMit;
+	}
+
+	public void setAnzMit(int anzMit) {
+		this.anzMit = anzMit;
+	}
+	
+	public Figure getFigureOnPosition(int position){
+		if(this.fieldArray[position] == null){
+			return null;
+		}
+	    return this.fieldArray[position];
+	}
+	public void setFigureOnPosition(Figure fig, int position){
+		if (fig != null)
+			fig.setFigurePos(position);
+	    this.fieldArray[position] = fig;
+	}
+	
+	public boolean isOccupied(int position) {
+	    if(this.fieldArray[position] == null) {
+		return false;
+	    }
+	    return true;
+	}	
+	
+	
+	public Player getPlayer(int playerID){
+	    return (players.get(playerID)) ;
+	}
+	public Figure[] getFieldArray(){
+		return fieldArray;
+	}
+	public Figure[] getTargetArray(int playerID){
+		switch(playerID){
+		case 0:
+			return target1;
+		case 1:
+			return target2;
+		case 2:
+			return target3;
+		case 3:
+			return target4;
+		default:
+			return null;
+		}
+	}
+	
+	public void storeFigure(Figure fig, int storagePoint){
+		int c = fig.getPlayerID();
+		switch(c)
+		{
+		case 0:
+			target1[storagePoint] = fig;
+			break;
+		case 1:
+			target2[storagePoint] = fig;
+			break;
+		case 2:
+			target3[storagePoint] = fig;
+			break;
+		case 3:
+			target4[storagePoint] = fig;
+			break;
+		}
+	}
+	
+}
