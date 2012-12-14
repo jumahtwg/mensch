cfda549b8d2eeed541296a66ed776955c4f9fbf0
Mathieu <marisserqhtwg-online.de>
2012-12-14 14:33:22 +0100

    FigureTest+PlayerTest updated

--- menschv2/test/model/FigureTest.java
+++ menschv2/test/model/FigureTest.java
@@ -13,15 +13,26 @@ public class FigureTest {
 	
 	@Before
 	public void setUp() {
-		fig = new Figure(10, plyr);
+		fig = new Figure(1, plyr = new Player(0));
 
 	}
 	
 	
 	@Test
 	public void test() {
-		assertEquals(0, fig.getWegLaenge());
-		assertEquals(10, fig.getFigureID());
+		assertEquals(0, fig.getWeglaenge());
+		assertEquals(1, fig.getFigureID());
+		assertEquals(0, fig.getFigurePos());
+		fig.setFigurePos(10);
+		assertEquals(10, fig.getFigurePos());
+		fig.setWeglaenge(12);
+		assertEquals(12, fig.getWeglaenge());
+		fig.resetWegLaenge();
+		assertEquals(0, fig.getWeglaenge());
+		assertEquals(0, fig.getPlayerID());
+		assertEquals(plyr, fig.hasPlayer());
+		assertEquals(0, fig.getPlayerID());
+		
 		
 	}
 
--- menschv2/test/model/PlayerTest.java
+++ menschv2/test/model/PlayerTest.java
@@ -1,26 +1,45 @@
 package model;
 
-import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
 
 public class PlayerTest {
 
-	Player plyr;
-	
+	Player plyr0;
+	Player plyr1;
+	Player plyr2;
+	Player plyr3;
+
 	@Before
 	public void setUp() {
-		plyr = new Player("Bob", 2);
-
+		plyr0 = new Player(0);
+		plyr1 = new Player(1);
+		plyr2 = new Player(2);
+		plyr3 = new Player(3);
 	}
 	
-	
+
 	@Test
 	public void test() {
-		assertEquals("Bob" , plyr.getName());
-		assertEquals(2, plyr.getPlayerID());
+		assertEquals(0 , plyr0.getPlayerID());
+		assertEquals(0, plyr0.getStartField());
+		
+		plyr0.pushFigure(plyr0.getFigure(0));
+		int x = plyr0.rolling();					
+		assertTrue(x < 7 && x > 0);
+		assertEquals(4, plyr0.getStackSize());
+		assertFalse(plyr0.figureStackEmpty());
+		for(int i=0; i< 4; i++){
+			plyr0.popFigure();
+		}
+		assertTrue(plyr0.figureStackEmpty());
+		
+		for(int i=0; i< 4; i++){
+			plyr0.storeFigure(plyr0.getFigure(i));
+		}
+		plyr0.getPgFigureArray();
 		
 	}
 
