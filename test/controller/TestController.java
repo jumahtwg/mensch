9e2bbeb5b3859a5f140101ecad4710339a00933a
Mathieu <marisserqhtwg-online.de>
2013-01-17 20:53:57 +0100

    TestController update

--- test/controller/TestController.java
+++ test/controller/TestController.java
@@ -7,6 +7,8 @@ import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 
+import controller.Controller.GAME_STATE;
+
 public class TestController extends TestCase {
 
 	Controller ctrl;
@@ -15,17 +17,43 @@ public class TestController extends TestCase {
 	public void setUp() {
 		ctrl = new Controller();
 		ctrl.setPl(2);
+		ctrl.start();
 		
 
 	}
 	@Test
 	public void test() throws NumberFormatException, IOException {
-		
-		ctrl.init();
+		ctrl.inputPlayerCount(ctrl.getPl());
+		for ( int i = 0; i < 100; i++) {
+			ctrl.doDice();
+		}
 		
 		assertEquals(2, ctrl.getPl()); 
+		assertEquals(2, ctrl.getAnzahlMitspieler());
 		assertEquals(4, Controller.getMaxspieler());
 		ctrl.comingOut(0);
+		ctrl.setStatus(GAME_STATE.ROLL);
+		assertEquals(GAME_STATE.ROLL, ctrl.getStatus());
+		ctrl.moveForward(ctrl.getFigureOnPos(0), 40);
+		ctrl.comingOut(0);
+		ctrl.moveForward(ctrl.getFigureOnPos(0), 41);
+		ctrl.comingOut(0);
+		ctrl.moveForward(ctrl.getFigureOnPos(0), 42);
+		ctrl.comingOut(0);
+		ctrl.moveForward(ctrl.getFigureOnPos(0), 43);
+		ctrl.getPlayerFigures();
+		ctrl.getActivePlayer();
+		ctrl.getPgArray();
+		ctrl.getRoll();
+		ctrl.getFieldCoords();
+		ctrl.getTargetCoords(0);
+		ctrl.getStackSize(0);
+		ctrl.getStackCoords();
+		ctrl.getTargetFigureArray(0);
+		ctrl.setPickFigure(0);
+		ctrl.update();
+		
+		
 	}
 
 }
